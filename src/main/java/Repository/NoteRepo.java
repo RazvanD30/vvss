79fa8c7a63586e9daa2c1841c9f01f63f1ff67bd
Razvan <razvandumitrascu3@gmail.com>
2019-04-17 09:21:24 +0300

    kill me pls x4

--- src/main/java/Repository/NoteRepo.java
+++ src/main/java/Repository/NoteRepo.java
@@ -27,87 +27,10 @@ public class NoteRepo extends AbstractRepo<Nota,Map.Entry<String,Integer>> {
         builderFactory=DocumentBuilderFactory.newInstance();
     }
     private void writeToFile(Nota s, String fd) throws IOException{
-        try{
-            DocumentBuilder db=builderFactory.newDocumentBuilder();
-            Document doc=db.newDocument();
-            Element r = doc.createElement("Note");
-            Element e = doc.createElement("Nota");
-            e.setAttribute("idNota",s.getID().getKey()+s.getID().getValue().toString());
-            Element grupa=doc.createElement("nr");
-            int n=s.getTema();
-            String grr=Integer.toString(n);
-            Text gr=doc.createTextNode(grr);
-            grupa.appendChild(gr);
-            e.appendChild(grupa);
-            Element numee=doc.createElement("valoare");
-            float val=s.getVal();
-            String vall=Float.toString(val);
-            Text num=doc.createTextNode(vall);
-            numee.appendChild(num);
-            e.appendChild(numee);
-            Element mail=doc.createElement("predat");
-            int pred=s.getPredat();
-            String prd=Integer.toString(pred);
-            Text email=doc.createTextNode(prd);
-            mail.appendChild(email);
-            e.appendChild(mail);
-            Element dd=doc.createElement("deadline");
-            int d=s.getDl();
-            String de=Integer.toString(d);
-            Text ded=doc.createTextNode(de);
-            dd.appendChild(ded);
-            e.appendChild(dd);
-            Element profesor=doc.createElement("feedback");
-            Text prof=doc.createTextNode(fd);
-            profesor.appendChild(prof);
-            e.appendChild(profesor);
-            r.appendChild(e);
-            doc.appendChild(r);
-            TransformerFactory tf=TransformerFactory.newInstance();
-            Transformer t=tf.newTransformer();
-            t.setOutputProperty(OutputKeys.INDENT, "yes");
-            t.setOutputProperty(OutputKeys.METHOD, "xml");
-            t.setOutputProperty(OutputKeys.ENCODING, "UTF-8");
-            t.setOutputProperty("{http://xml.apache.org/xslt}indent-amount", "4");
-            t.transform(new DOMSource(doc),new StreamResult(new FileOutputStream(s.getStudent().getNume()+".xml",true)));
-        }
-        catch (Exception e){e.printStackTrace();}
+
     }
     private void writeToFile2(Nota s) throws IOException{
-        try{
-            DocumentBuilder db=builderFactory.newDocumentBuilder();
-            Document doc=db.newDocument();
-            Element r = doc.createElement("Note");
-            Element e = doc.createElement("Nota");
-            e.setAttribute("idNota",s.getID().getKey()+s.getID().getValue().toString());
-            Element st=doc.createElement("nume");
-            String stu=s.getStudent().getNume();
-            Text std=doc.createTextNode(stu);
-            st.appendChild(std);
-            e.appendChild(st);
-            Element grupa=doc.createElement("nr");
-            int n=s.getTema();
-            String grr=Integer.toString(n);
-            Text gr=doc.createTextNode(grr);
-            grupa.appendChild(gr);
-            e.appendChild(grupa);
-            Element numee=doc.createElement("valoare");
-            float val=s.getVal();
-            String vall=Float.toString(val);
-            Text num=doc.createTextNode(vall);
-            numee.appendChild(num);
-            e.appendChild(numee);
-            r.appendChild(e);
-            doc.appendChild(r);
-            TransformerFactory tf=TransformerFactory.newInstance();
-            Transformer t=tf.newTransformer();
-            t.setOutputProperty(OutputKeys.INDENT, "yes");
-            t.setOutputProperty(OutputKeys.METHOD, "xml");
-            t.setOutputProperty(OutputKeys.ENCODING, "UTF-8");
-            t.setOutputProperty("{http://xml.apache.org/xslt}indent-amount", "4");
-            t.transform(new DOMSource(doc),new StreamResult(new FileOutputStream("Catalog.xml",true)));
-        }
-        catch (Exception e){e.printStackTrace();}
+
     }
     public Nota save(Nota el,String fd) {
         AtomicInteger ok= new AtomicInteger();
@@ -121,8 +44,10 @@ public class NoteRepo extends AbstractRepo<Nota,Map.Entry<String,Integer>> {
         if(ok.get() ==0) {
             Nota t = super.save(el);
             try {
-                writeToFile(el, fd);
-                writeToFile2(el);
+                if( false ) {
+                    writeToFile(el, fd);
+                    writeToFile2(el);
+                }
             } catch (IOException ex) {
                 System.out.println(ex);
             }
